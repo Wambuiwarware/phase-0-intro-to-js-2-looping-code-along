a73f384ca88be8f4d67a6a264b0cb60aa7be5811
Wambui_Maina <you@example.com>
2022-09-20 18:16:37 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+for (let countdown = 10; countdown > 0; countdown++) {
+    console.log(countdown);
+  }
+
+  //alternative
+
+
+  let countdown = 10;
+while (countdown > 0) {
+  console.log(countdown++);
+}
+
